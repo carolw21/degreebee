d15f97e49b6c4a21751b24bb2cb6930e1feb1cb9
carolw21 <carolw2000@gmail.com>
2016-03-26 20:06:46 +0000

    All files.

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+function animateCircle(speed, radius, startx, starty, phi) {  
+    if (!phi) { phi = 0 };
+    var int = 2 * (Math.PI) / speed;
+    phi = (phi >= 2 * (Math.PI)) ? 0 : (phi + int);
+    var $m = startx + (radius+250) * Math.cos(phi);
+    var $n = starty + radius * Math.sin(phi);
+
+    $('#bee').animate({
+        marginLeft: $m + 'px',
+        marginTop: $n + 'px'
+      }, 1, function() { 
+             animateCircle.call(this, speed, radius, startx, starty, phi) 
+          }
+    );
+
+};
+
+$(document).ready(function() {
+    animateCircle(200, 150, 10, -60);
+});
